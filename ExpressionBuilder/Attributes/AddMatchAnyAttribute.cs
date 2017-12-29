0f3955b4300a086033ffcd29a314e6ed652a3225
Glenn <33450392+glenn2223@users.noreply.github.com>
2017-12-29 15:10:49 +0000

    Attribute to allow Match Any
    Attribute that must be added to any fields/properties to allow the MatchAny operation.

---
+++ ExpressionBuilder/Attributes/AddMatchAnyAttribute.cs
@@ -0,0 +1,20 @@
+using System;
+using System.ComponentModel;
+
+namespace ExpressionBuilder.Attributes
+{
+    internal class AddMatchAnyAttribute : Attribute
+    {
+        [DefaultValue(false)]
+        public bool AddMatchAny { get; private set; }
+
+        /// <summary>
+        /// Defines weather the <see cref="Common.Operation.MatchAny"/> attribute is allowed for the specified field or property.
+        /// </summary>
+        /// <param name="allow">Is MatchAny allowed.</param>
+        public AddMatchAnyAttribute(bool allow = false)
+        {
+            AddMatchAny = allow;
+        }
+    }
+}
