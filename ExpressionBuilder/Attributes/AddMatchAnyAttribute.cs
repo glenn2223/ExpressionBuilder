9d21c66cb8205a4b2c2be693217bdaee38e20b6c
Glenn <33450392+glenn2223@users.noreply.github.com>
2017-12-29 17:03:01 +0000

    Update AddMatchAnyAttribute.cs

--- ExpressionBuilder/Attributes/AddMatchAnyAttribute.cs
+++ ExpressionBuilder/Attributes/AddMatchAnyAttribute.cs
@@ -6,34 +6,3 @@ namespace ExpressionBuilder.Attributes
     {
     }
 }
-
-
-/*
-/// OLD NOTE
-
-/// Irrelivant: [WAIT, MAYBE NOT => see last line]
-/// Now checking if <see cref="ExpressionBuilder.Interfaces.IFilterStatement.Value"/> is an array
-/// Might use this as a way to add the operation while building the winform [STAND BY]
-
-
-using System;
-using System.ComponentModel;
-
-namespace ExpressionBuilder.Attributes
-{
-    public class AddMatchAnyAttribute : Attribute
-    {
-        [DefaultValue(false)]
-        public bool AddMatchAny { get; private set; }
-
-        /// <summary>
-        /// Defines weather the <see cref="Common.Operation.MatchAny"/> attribute is allowed for the specified field or property.
-        /// </summary>
-        /// <param name="allow">Is MatchAny allowed.</param>
-        public AddMatchAnyAttribute(bool allow = false)
-        {
-            AddMatchAny = allow;
-        }
-    }
-}
-*/
