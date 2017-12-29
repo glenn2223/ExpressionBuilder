93185a0fecc6231e991de455185cc685217ad877
Glenn <33450392+glenn2223@users.noreply.github.com>
2017-12-29 16:58:16 +0000

    Update AddMatchAnyAttribute.cs

--- ExpressionBuilder/Attributes/AddMatchAnyAttribute.cs
+++ ExpressionBuilder/Attributes/AddMatchAnyAttribute.cs
@@ -1,8 +1,20 @@
+using System;
+
+namespace ExpressionBuilder.Attributes
+{
+    public class AddMatchAny : Attribute
+    {
+    }
+}
+
+
 /*
+/// OLD NOTE
+
 /// Irrelivant: [WAIT, MAYBE NOT => see last line]
 /// Now checking if <see cref="ExpressionBuilder.Interfaces.IFilterStatement.Value"/> is an array
 /// Might use this as a way to add the operation while building the winform [STAND BY]
-*/
+
 
 using System;
 using System.ComponentModel;
@@ -24,3 +36,4 @@ namespace ExpressionBuilder.Attributes
         }
     }
 }
+*/
