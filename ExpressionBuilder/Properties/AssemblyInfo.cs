059192385f6c741f35252dbb02be974f141a6141
Glenn <33450392+glenn2223@users.noreply.github.com>
2018-01-18 09:48:11 +0000

    Nuget Prep

--- ExpressionBuilder/Properties/AssemblyInfo.cs
+++ ExpressionBuilder/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.2.*")]
-[assembly: AssemblyInformationalVersion("1.1.2-rc")]
+[assembly: AssemblyVersion("1.2.0.*")]
+[assembly: AssemblyInformationalVersion("1.2.0-rc")]
